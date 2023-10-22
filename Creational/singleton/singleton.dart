f6698c8a7bebae584946787e57d97b86482038b4
AbdllahMohamed <dev.abdo2000@gmail.com>
2023-10-23 01:58:25 +0300

    SingleTon

---
+++ Creational/singleton/singleton.dart
@@ -0,0 +1,10 @@
+class SingleTon {
+  static SingleTon _instance = SingleTon._internal();
+
+  SingleTon._internal();
+
+// factory constructor should return same instance
+  factory SingleTon() {
+    return _instance;
+  }
+}
